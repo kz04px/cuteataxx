416ee818bc5a515bbe64dd6db44c78434bf9326b
kz04px <6536046+kz04px@users.noreply.github.com>
2022-07-02 23:15:48 +0100

    Openings file basic sanity check and comment support

--- src/cli/parse/openings.cpp
+++ src/cli/parse/openings.cpp
@@ -9,12 +9,17 @@ namespace parse {
     std::vector<std::string> openings;
 
     if (!f.is_open()) {
-        openings.push_back("x5o/7/7/7/7/7/o5x x 0");
+        openings.push_back("x5o/7/7/7/7/7/o5x x 0 1");
         return openings;
     }
 
+    const auto min_fen_size = std::string("7/7/7/7/7/7/7").size();
+
     std::string line;
     while (std::getline(f, line)) {
+        if (line.size() < min_fen_size || line[0] == '#') {
+            continue;
+        }
         openings.push_back(line);
     }
 
