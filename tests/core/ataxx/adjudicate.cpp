219d53392a277e19020a737e4cf76b02c04897ad
kz04px <6536046+kz04px@users.noreply.github.com>
2023-03-06 00:46:55 +0000

    Add adjudication tests

---
+++ tests/core/ataxx/adjudicate.cpp
@@ -0,0 +1,34 @@
+#include "core/ataxx/adjudicate.hpp"
+#include <doctest/doctest.h>
+#include <array>
+#include <libataxx/move.hpp>
+#include <string>
+
+TEST_CASE("Adjudicate - easyfill true") {
+    const std::array<std::string, 4> fens = {
+        "xoo4/ooo4/ooo4/7/7/7/7 x 0 1",
+        "oxx4/xxx4/xxx4/7/7/7/7 o 0 1",
+        "x--4/ooo4/ooo4/7/7/7/7 x 0 1",
+        "o--4/xxx4/xxx4/7/7/7/7 o 0 1",
+    };
+
+    for (const auto &fen : fens) {
+        const auto pos = libataxx::Position(fen);
+        REQUIRE(can_adjudicate_easyfill(pos));
+    }
+}
+
+TEST_CASE("Adjudicate - easyfill false") {
+    const std::array<std::string, 5> fens = {
+        "startpos",
+        "7/7/7/7/7/7/7 x 0 1",
+        "7/7/7/7/7/7/7 o 0 1",
+        "xoo4/o1o4/ooo4/7/7/7/7 x 0 1",
+        "oxx4/x1x4/xxx4/7/7/7/7 o 0 1",
+    };
+
+    for (const auto &fen : fens) {
+        const auto pos = libataxx::Position(fen);
+        REQUIRE(!can_adjudicate_easyfill(pos));
+    }
+}
