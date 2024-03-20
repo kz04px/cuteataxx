109d090a555cb01b454c6a8fddfac52a1239a335
kz04px <6536046+kz04px@users.noreply.github.com>
2024-03-20 03:03:03 +0000

    Rework round robin mixed for odd numbers of players

--- src/core/tournament/roundrobin_mixed.hpp
+++ src/core/tournament/roundrobin_mixed.hpp
@@ -14,11 +14,15 @@ class [[nodiscard]] RoundRobinMixedGenerator final : public TournamentGenerator 
                              const std::size_t openings,
                              const bool r)
         : num_players(players), num_games(games), num_openings(openings), repeat(r) {
-        for (std::size_t i = 0; i < players; ++i) {
+        magic.emplace_back(0);
+        for (std::size_t i = 2; i < players + (players % 2); ++i) {
             magic.emplace_back(i);
         }
+        magic.emplace_back(1);
+
         player1 = 0;
-        player2 = num_players - 1;
+        player2 = 1;
+        opening = num_openings - 1;
     }
 
     virtual ~RoundRobinMixedGenerator() {
@@ -34,57 +38,64 @@ class [[nodiscard]] RoundRobinMixedGenerator final : public TournamentGenerator 
     }
 
     [[nodiscard]] virtual auto next() -> GameInfo override {
-        const auto result = GameInfo{idx, opening, player1, player2};
         increment();
-        return result;
+        const auto is_mirror = repeat && idx % 2 == 0;
+        if (is_mirror) {
+            return GameInfo{idx - 1, opening, magic.at(player2), magic.at(player1)};
+        } else {
+            return GameInfo{idx - 1, opening, magic.at(player1), magic.at(player2)};
+        }
     }
 
    private:
     virtual auto increment() -> void override {
-        idx++;
-
         if (repeat) {
-            const auto is_new_round = idx % (num_players) == 0;
-            if (is_new_round) {
-                std::rotate(magic.rbegin(), magic.rbegin() + 1, magic.rend() - 1);
-            }
-
-            if (idx % 2) {
-                player1 = magic.at(num_players - 1 - ((idx / 2) % (num_players / 2)));
-                player2 = magic.at((idx / 2) % (num_players / 2));
-            } else {
-                player1 = magic.at((idx / 2) % (num_players / 2));
-                player2 = magic.at(num_players - 1 - ((idx / 2) % (num_players / 2)));
+            if (idx % 2 == 0) {
+                player1++;
+                player2--;
                 opening++;
             }
         } else {
-            const auto is_new_round = idx % (num_players / 2) == 0;
-            if (is_new_round) {
-                std::rotate(magic.rbegin(), magic.rbegin() + 1, magic.rend() - 1);
-            }
-
-            player1 = magic.at(idx % (num_players / 2));
-            player2 = magic.at(num_players - 1 - (idx % (num_players / 2)));
+            player1++;
+            player2--;
             opening++;
         }
 
+        auto okay = true;
+        do {
+            okay = true;
+
+            // New round?
+            if (player1 >= player2) {
+                std::rotate(magic.rbegin(), magic.rbegin() + 1, magic.rend() - 1);
+                player1 = 0;
+                player2 = magic.size() - 1;
+                okay = false;
+            }
+
+            // Bye?
+            const auto is_bye = magic.at(player1) == num_players || magic.at(player2) == num_players;
+            if (is_bye) {
+                player1++;
+                player2--;
+                okay = false;
+            }
+        } while (!okay);
+
         opening = opening % num_openings;
 
-        assert(player1 < num_players);
-        assert(player2 < num_players);
+        idx++;
     }
 
     std::size_t num_players = 0;
     std::size_t num_games = 0;
     std::size_t num_openings = 0;
-    bool repeat = true;
-    // state
     std::size_t idx = 0;
     std::size_t opening = 0;
     std::size_t player1 = 0;
     std::size_t player2 = 1;
-    std::size_t match_game = 0;
     std::vector<std::size_t> magic;
+    bool repeat = true;
 };
 
 #endif
--- tests/core/tournament/roundrobin_mixed.cpp
+++ tests/core/tournament/roundrobin_mixed.cpp
@@ -201,4 +201,112 @@ TEST_SUITE("Tournament - Round Robin Mixed") {
         REQUIRE(gen.next() == GameInfo{28, 0, 2, 5});
         REQUIRE(gen.next() == GameInfo{29, 1, 3, 4});
     }
+
+    TEST_CASE("Test odd player count") {
+        const auto num_players = 3;
+        const auto num_games = 2;
+        const auto num_openings = 2;
+        auto gen = RoundRobinMixedGenerator(num_players, num_games, num_openings, true);
+
+        REQUIRE(gen.expected() == 6);
+
+        // id, opening, player1, player2
+        REQUIRE(gen.next() == GameInfo{0, 0, 1, 2});
+        REQUIRE(gen.next() == GameInfo{1, 0, 2, 1});
+        REQUIRE(gen.next() == GameInfo{2, 1, 0, 2});
+        REQUIRE(gen.next() == GameInfo{3, 1, 2, 0});
+        REQUIRE(gen.next() == GameInfo{4, 0, 0, 1});
+        REQUIRE(gen.next() == GameInfo{5, 0, 1, 0});
+
+        // Overflow
+        REQUIRE(gen.next() == GameInfo{6, 1, 1, 2});
+        REQUIRE(gen.next() == GameInfo{7, 1, 2, 1});
+        REQUIRE(gen.next() == GameInfo{8, 0, 0, 2});
+        REQUIRE(gen.next() == GameInfo{9, 0, 2, 0});
+        REQUIRE(gen.next() == GameInfo{10, 1, 0, 1});
+        REQUIRE(gen.next() == GameInfo{11, 1, 1, 0});
+    }
+
+    TEST_CASE("Test odd player count 2") {
+        const auto num_players = 3;
+        const auto num_games = 4;
+        const auto num_openings = 5;
+        auto gen = RoundRobinMixedGenerator(num_players, num_games, num_openings, true);
+
+        REQUIRE(gen.expected() == 12);
+
+        // id, opening, player1, player2
+        REQUIRE(gen.next() == GameInfo{0, 0, 1, 2});
+        REQUIRE(gen.next() == GameInfo{1, 0, 2, 1});
+        REQUIRE(gen.next() == GameInfo{2, 1, 0, 2});
+        REQUIRE(gen.next() == GameInfo{3, 1, 2, 0});
+        REQUIRE(gen.next() == GameInfo{4, 2, 0, 1});
+        REQUIRE(gen.next() == GameInfo{5, 2, 1, 0});
+
+        REQUIRE(gen.next() == GameInfo{6, 3, 1, 2});
+        REQUIRE(gen.next() == GameInfo{7, 3, 2, 1});
+        REQUIRE(gen.next() == GameInfo{8, 4, 0, 2});
+        REQUIRE(gen.next() == GameInfo{9, 4, 2, 0});
+        REQUIRE(gen.next() == GameInfo{10, 0, 0, 1});
+        REQUIRE(gen.next() == GameInfo{11, 0, 1, 0});
+
+        // Overflow
+        REQUIRE(gen.next() == GameInfo{12, 1, 1, 2});
+        REQUIRE(gen.next() == GameInfo{13, 1, 2, 1});
+        REQUIRE(gen.next() == GameInfo{14, 2, 0, 2});
+        REQUIRE(gen.next() == GameInfo{15, 2, 2, 0});
+        REQUIRE(gen.next() == GameInfo{16, 3, 0, 1});
+        REQUIRE(gen.next() == GameInfo{17, 3, 1, 0});
+
+        REQUIRE(gen.next() == GameInfo{18, 4, 1, 2});
+        REQUIRE(gen.next() == GameInfo{19, 4, 2, 1});
+        REQUIRE(gen.next() == GameInfo{20, 0, 0, 2});
+        REQUIRE(gen.next() == GameInfo{21, 0, 2, 0});
+        REQUIRE(gen.next() == GameInfo{22, 1, 0, 1});
+        REQUIRE(gen.next() == GameInfo{23, 1, 1, 0});
+    }
+
+    TEST_CASE("Basic sanity") {
+        for (auto num_players = 2; num_players < 6; ++num_players) {
+            for (auto num_games = 1; num_games < 20; ++num_games) {
+                for (auto num_openings = 1; num_openings < 10; ++num_openings) {
+                    for (const auto repeat : {false, true}) {
+                        auto gen = RoundRobinMixedGenerator(num_players, num_games, num_openings, repeat);
+                        auto last = GameInfo{};
+
+                        REQUIRE(gen.expected() > 0);
+
+                        if (num_games % 2 == 0) {
+                            REQUIRE(gen.expected() % 2 == 0);
+                        }
+
+                        for (std::size_t i = 0; i < gen.expected(); ++i) {
+                            const auto info = gen.next();
+
+                            REQUIRE(info.id == i);
+                            REQUIRE(info.idx_opening <= num_openings);
+                            REQUIRE(info.idx_player1 < num_players);
+                            REQUIRE(info.idx_player2 < num_players);
+                            REQUIRE(info.idx_player1 != info.idx_player2);
+
+                            if (i > 0) {
+                                REQUIRE(info.id == last.id + 1);
+
+                                if (repeat && i % 2 == 1) {
+                                    REQUIRE(info.idx_opening == last.idx_opening);
+                                    REQUIRE(info.idx_player1 == last.idx_player2);
+                                    REQUIRE(info.idx_player2 == last.idx_player1);
+                                }
+                            } else {
+                                REQUIRE(info.id == 0);
+                                REQUIRE(info.idx_opening == 0);
+                            }
+
+                            last = info;
+                        }
+                    }
+                }
+            }
+        }
+    }
 }
