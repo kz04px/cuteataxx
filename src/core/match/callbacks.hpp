5d3b83036f707aecbbf041eca4e5ddc24c07154c
tsoj <tsoj.tsoj@gmx.de>
2024-05-23 22:39:11 +0200

    Update callbacks.hpp

--- src/core/match/callbacks.hpp
+++ src/core/match/callbacks.hpp
@@ -5,11 +5,12 @@
 #include <libataxx/move.hpp>
 #include <string>
 #include "results.hpp"
+#include <libataxx/position.hpp>
 
 struct Callbacks {
     std::function<void(const std::string &)> on_engine_start;
     std::function<void(const int, const std::string &, const std::string &, const std::string &)> on_game_started;
-    std::function<void(const int, const std::string &, const std::string &)> on_game_finished;
+    std::function<void(const libataxx::Result, const std::string &, const std::string &)> on_game_finished;
     std::function<void(const Results &)> on_results_update;
     std::function<void(const std::string &)> on_info_send;
     std::function<void(const std::string &)> on_info_recv;
